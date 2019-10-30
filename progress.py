16f63aabb2ace19c7afa10ac56e2c9517f5016d0
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-10-30 19:48:01 +0300

    Implement advanced progress reporting (with estimated finish time)

--- progress.py
+++ progress.py
@@ -16,22 +16,37 @@
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+import time
+from datetime import timedelta
 from typing import Any, Iterable
 
 
 def progressify(iterable: Any, message: str) -> Iterable[Any]:
-    print('{}...'.format(message), end='\r', file=sys.stderr)
+    num_total = len(iterable) if hasattr(iterable, '__len__') else None
+    num_done = 0
+    it = iter(iterable)
+    start_time = time.monotonic()
 
-    count = len(iterable) if hasattr(iterable, '__len__') else None
+    def print_progress(end: str) -> None:
+        if num_total is None:
+            print('{} [{}]...'.format(message, num_done), end=end, file=sys.stderr)
+            return
 
-    def print_progress(num: int, end: str) -> None:
-        if count is None:
-            print('{} [{}]...'.format(message, num), end=end, file=sys.stderr)
-        else:
-            print('{} [{}/{}]...'.format(message, num, count), end=end, file=sys.stderr)
+        elapsed_seconds = time.monotonic() - start_time
 
-    for num, value in enumerate(iterable, 1):
-        print_progress(num, '\r')
-        yield value
+        if elapsed_seconds >= 1.0 and num_done >= 1:
+            remaining_seconds = elapsed_seconds * (num_total - num_done) / num_done
+            print('{} [{}/{}, {} remaining]... '.format(message, num_done, num_total, timedelta(seconds=int(remaining_seconds))), end=end, file=sys.stderr)
+            return
 
-    print_progress(num, '\n')
+        print('{} [{}/{}]...'.format(message, num_done, num_total), end=end, file=sys.stderr)
+
+    while True:
+        print_progress('\r')
+
+        try:
+            yield next(it)
+            num_done += 1
+        except StopIteration:
+            print_progress('\n')  # end with # of items
+            return
