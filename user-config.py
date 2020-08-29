026d871c82348de81592e08f94426637f686fcb2
Dmitry Marakasov <amdmi3@amdmi3.ru>
2020-08-30 02:56:32 +0300

    Tweak maxlag and retry settings

--- user-config.py
+++ user-config.py
@@ -2,5 +2,6 @@ mylang = 'wikidata'
 family = 'wikidata'
 usernames['wikidata']['wikidata'] = 'Repology_bot'
 noisysleep = 999999.0
-maxlag = 60
+maxlag = 5
 socket_timeout = (5, 120)
+max_retries = 99999
